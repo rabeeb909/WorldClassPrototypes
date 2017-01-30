3c43060dac50192110b8a59a3289ec091fa5a05c
rabeeb909 <rabeebanwar@gmail.com>
2017-01-29 19:16:01 -0500

    made WorldCP file

---
+++ WorldCP/index.php
@@ -0,0 +1,16 @@
+</html>
+
+<head> 
+<title>World Class Prototypes</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+
+<div id="pagee-wrap">
+	<h1>World Class Prototypes</h1>
+	<p>Website</p>
+
+</body>
+
+</html>
