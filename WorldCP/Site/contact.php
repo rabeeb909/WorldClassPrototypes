9a303cf462b2961c4557e10c9153bcf31016c0ba
rabeeb909 <rabeebanwar@gmail.com>
2017-03-31 20:07:06 -0400

    added code for form input to google spreadsheet

--- WorldCP/Site/contact.php
+++ WorldCP/Site/contact.php
@@ -1,4 +1,4 @@
-
+<?php $xml=simplexml_load_file("contact.xml") or die("Error: Cannot create object"); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -63,12 +63,13 @@
           style="visibility: visible; animation-name: fadeInDown; -webkit-animation-name: fadeInDown;">Contact Us</h2>
 
       <p class="wow fadeInDown animated"
-         style="visibility: visible; animation-name: fadeInDown; -webkit-animation-name: fadeInDown;">We always welcome feedback. Below is our contact information and also a feedback form.<br></p>
+         style="visibility: visible; animation-name: fadeInDown; -webkit-animation-name: fadeInDown;"><?php echo $xml->title; ?><br></p>
     </div>
 <script src='https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBNVLOeDktpd5jW1bfUdsW__zMNKOjUBK8'></script><div style='overflow:hidden;height:285px;width:722px;'><div id='gmap_canvas' style='height:285px;width:722px;'></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div> <a href='https://www.embedmap.net/'>google map embed</a> <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=32e22ebb5eaa6f0046faeb6e4d0a426494d7c42e'></script><script type='text/javascript'>function init_map(){var myOptions = {zoom:15,center:new google.maps.LatLng(42.804221,-86.10038350000002),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(42.804221,-86.10038350000002)});infowindow = new google.maps.InfoWindow({content:'<strong>World Class Prototypes, Inc.</strong><br>243 129th Avenue<br>49424 Holland<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
   </div>
 </section>
-<h3><?php include "feedback_email.php"?></h3>
+<!-- <h3><?php include "feedback_email.php"?></h3>
+-->
 <section id="contact">
 
   <div class="container">
@@ -91,20 +92,21 @@
 
             <form id="main-contact-form" name="contact-form" method="post" action="#">
               <div class="form-group">
-                <input type="text" name="vname" class="form-control" placeholder="Name" required="">
+                <input type="text" name="name" class="form-control" placeholder="Name" required="">
               </div>
               <div class="form-group">
-                <input type="text" name="vemail" class="form-control" placeholder="Email" required="">
+                <input type="text" name="email" class="form-control" placeholder="Email" required="">
               </div>
               <div class="form-group">
-                <input type="text" name="sub" class="form-control" placeholder="Subject" required="">
+                <input type="text" name="subject" class="form-control" placeholder="Subject" required="">
               </div>
               <div class="form-group">
-                <textarea name="msg" class="form-control" rows="8" placeholder="Message" required=""></textarea>
+                <textarea name="message" class="form-control" rows="8" placeholder="Message" required=""></textarea>
               </div>
               <button type="submit" name="submit" class="btn btn-primary">Send Message</button>
             </form>
-
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	       <script src='google-sheet.js'></script>
           </div>
         </div>
       </div>
