8c7030c0dcc13984d458570d7a6737ab331918c6
Jordan Zomerlei <jzomerlei@gmail.com>
2017-04-02 15:52:28 -0400

    Carousels added and made the tool bar consistent

---
+++ WorldCP/mail_order.php
@@ -0,0 +1,64 @@
+<?php
+if(isset($_POST["Submit"])){
+
+$to = "anwarr@mail.gvsu.edu";
+$subject= "test attachment";
+$todayis = date("l, F j, Y, g:i a") ;
+
+$message = "
+Date : $todayis
+Contact Name : $contactName
+Company Name : $companyName
+Email : $email
+Phone Number : $phone
+
+Process : $process
+Material : $material
+Quantity : $quantity
+Finish Level : $finishLevel
+Additional Infomation : $additionalInfo
+";
+
+$mime_boundary="==Multipart_Boundary_x".md5(mt_rand())."x";
+         $headers = "From: $email\r\n" .
+         "MIME-Version: 1.0\r\n" .
+            "Content-Type: multipart/mixed;\r\n" .
+            " boundary=\"{$mime_boundary}\"";
+         $message = "This is a multi-part message in MIME format.\n\n" .
+            "--{$mime_boundary}\n" .
+            "Content-Type: text/plain; charset=\"iso-8859-1\"\n" .
+            "Content-Transfer-Encoding: 7bit\n\n" .
+         $message . "\n\n";
+         foreach($_FILES as $userfile)
+         {
+            $tmp_name = $userfile['tmp_name'];
+            $type = $userfile['type'];
+            $name = $userfile['name'];
+            $size = $userfile['size'];
+            if (file_exists($tmp_name))
+            {
+               if(is_uploaded_file($tmp_name))
+               {
+                  $file = fopen($tmp_name,'rb');
+                  $data = fread($file,filesize($tmp_name));
+                  fclose($file);
+                  $data = chunk_split(base64_encode($data));
+               }
+               $message .= "--{$mime_boundary}\n" .
+                  "Content-Type: {$type};\n" .
+                  " name=\"{$name}\"\n" .
+                  "Content-Disposition: attachment;\n" .
+                  " filename=\"{$fileatt_name}\"\n" .
+                  "Content-Transfer-Encoding: base64\n\n" .
+               $data . "\n\n";
+            }
+         }
+         $message.="--{$mime_boundary}--\n";
+if (mail($to, $subject, $message, $headers))
+   echo "Mail sent successfully.";
+else
+   echo "Error in mail";
+
+}
+
+?>
